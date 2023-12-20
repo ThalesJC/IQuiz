60872ced16388ef9d8abeafd89ba6e8601071f34
ThalesJC <thales.soul96@gmail.com>
2023-12-20 08:07:14 -0300

    Initial Commit

---
+++ IQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IQuiz
+//
+//  Created by Thales J Colovini on 20/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
